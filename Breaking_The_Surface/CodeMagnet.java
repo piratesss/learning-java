2f4a0ce92922608c5070445514620dbfc394e935
shubham-tam <tamrakarshubham23@gmail.com>
2024-08-16 21:26:25 +0545

    code magenet problem added

---
+++ Breaking_The_Surface/CodeMagnet.java
@@ -0,0 +1,27 @@
+package Breaking_The_Surface;
+
+public class CodeMagnet {
+    public static void main(String[] args) {
+        int x = 3;
+
+        while (x > 0) {
+
+            if (x > 2) {
+                System.out.print('a');
+                System.out.print('-');
+            }
+            
+            if (x == 2) {
+                System.err.print("b c");
+                System.out.print('-');
+            }
+
+            if (x == 1) {
+                System.out.print("d");
+                x = x - 1;
+            }
+            x = x - 1;
+
+        }
+    }
+}
