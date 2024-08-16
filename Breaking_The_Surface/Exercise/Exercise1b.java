49e8d3b0b88fa48e1f65fb3a3429543f6f7b553c
shubham-tam <tamrakarshubham23@gmail.com>
2024-08-16 23:24:30 +0545

    Exercise folder added

---
+++ Breaking_The_Surface/Exercise/Exercise1b.java
@@ -0,0 +1,17 @@
+package Breaking_The_Surface.Exercise;
+
+class Exercise1b {
+
+    public static void main(String[] args) {
+        int x = 5;
+
+        while (x > 1) {
+            x = x - 1;
+            if (x < 3) {
+                System.out.println("small x");
+            }
+
+        }
+    }
+
+}
---
+++ Breaking_The_Surface/Test.java
@@ -0,0 +1,17 @@
+package Breaking_The_Surface;
+
+class Test {
+
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 0;
+        while (x < 5) {
+
+            // y = x - y; // 00 11 21 32 42
+            System.out.print(x + "" + y + " ");
+            x = x + 1;
+
+        }
+    }
+
+}
